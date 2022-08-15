95be17ff445721bc7cb90f5be3429fd72e3e4288
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2022-08-15 16:10:14 +0545

    Add userform component

---
+++ src/components/ChatPopup/common/Support/common/UserForm/index.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+
+// components
+import Input from '@/components/Input';
+
+// packages
+import { useForm } from 'react-hook-form';
+import Dropdown from '@/components/Dropdown';
+import Button from '@/components/Button';
+
+const formData = [
+  {
+    name: 'name',
+    label: 'Name',
+    placeholder: 'Enter your full name',
+    error: 'Please enter your full name',
+  },
+  {
+    name: 'email',
+    label: 'Email address',
+    placeholder: 'Enter your email address',
+    error: 'Please enter your email address',
+    type: 'email',
+  },
+  {
+    name: 'customer',
+    label: 'Are you a customer?',
+    dropDownItems: [
+      {
+        value: 'customer',
+        label: 'Customer',
+      },
+      {
+        value: 'company',
+        label: 'Company',
+      },
+      {
+        value: 'visitor',
+        label: 'Visitor',
+      },
+    ],
+  },
+  {
+    name: 'message',
+    label: 'Message',
+    placeholder: 'Ask a question here...',
+    error: 'Please enter your message',
+  },
+];
+
+const UserForm = () => {
+  const {
+    reset,
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleUserCreate = (data: any) => {
+    console.log({ data });
+    reset();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => handleUserCreate(data))}
+      className='flex flex-col'
+    >
+      <p className='mb-6 text-neutral-900'>
+        Enter your information below and our team will contact you shortly.
+      </p>
+      {formData.map((formItem) => {
+        if (formItem.dropDownItems) {
+          return (
+            <Dropdown
+              key={formItem.name}
+              control={control}
+              name={formItem.name}
+              label={formItem.label}
+              errorMessage={errors?.[formItem.name]?.message}
+              hasError={Boolean(errors?.[formItem.name])}
+              dropDownItems={formItem.dropDownItems}
+              defaultItem={formItem.dropDownItems[0]}
+            />
+          );
+        } else {
+          return (
+            <Input
+              key={formItem.name}
+              name={formItem.name}
+              label={formItem.label}
+              placeholder={formItem.placeholder}
+              hasError={Boolean(errors[formItem.name])}
+              errorMessage={errors.error}
+              register={register}
+              type={formItem.type || 'text'}
+            />
+          );
+        }
+      })}
+      <div className='flex flex-row justify-between gap-4 mt-2'>
+        <Button
+          title='Skip'
+          buttonType='outline'
+          size='small'
+          containerStyles='flex-1'
+          titleStyles='text-s'
+        />
+        <Button
+          title='Send'
+          size='small'
+          containerStyles='flex-1'
+          titleStyles='text-s'
+        />
+      </div>
+    </form>
+  );
+};
+
+export default UserForm;
--- src/components/ChatPopup/common/Support/index.tsx
+++ src/components/ChatPopup/common/Support/index.tsx
@@ -1,8 +1,11 @@
+// common
+import UserForm from './common/UserForm';
+
 const Support = () => {
   return (
     <div className='bg-neutral-100 p-6'>
       <div className='h-max-[500px] rounded-lg shadow-main p-6'>
-        <p>Chat message</p>
+        <UserForm />
       </div>
     </div>
   );
