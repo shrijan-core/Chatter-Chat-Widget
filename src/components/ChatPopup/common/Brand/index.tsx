9210fad54822f7f51a35ef6a37d1ebfc260f917e
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2022-08-15 12:09:16 +0545

    Add chat footer/brand section

---
+++ src/components/ChatPopup/common/Brand/index.tsx
@@ -0,0 +1,9 @@
+const Brand = () => {
+  return (
+    <div className='py-4 bg-neutral-200'>
+      <p className='text-neutral-600 text-s text-center'>Powered by Chatter</p>
+    </div>
+  );
+};
+
+export default Brand;
