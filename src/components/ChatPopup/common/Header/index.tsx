714a40222ba32efb8dac094b66f9d0dbd5daeb90
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2022-08-15 12:09:33 +0545

    Add chat header section

---
+++ src/components/ChatPopup/common/Header/index.tsx
@@ -0,0 +1,13 @@
+const ChatHeader = () => {
+  return (
+    <div className='px-10 py-5 bg-primary-500 text-neutral-100'>
+      <p className='text-h5 mb-2 text-neutral-100'>Hello,</p>
+      <p className='text-p'>
+        We help your business grow by connecting you to your customers. Leave us
+        a message.
+      </p>
+    </div>
+  );
+};
+
+export default ChatHeader;
